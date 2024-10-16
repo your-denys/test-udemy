ac01d7067412845e2ab45ede2d830b75333f6da9
Denys <denzfg@gmail.com>
2024-10-15 20:10:04 -0400

    Udemy

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+a = 14;
+console.log(a);
+
+let Numb = 5;
+let numb = 5;
+numb = 3;
+
+const leftBorderWidth = 1;
+leftBorderWidth = 3; // ошибка
+
+const rightBorderWidth = {
+  belower: 1,
+};
+
+rightBorderWidth.belower = 3;
+
+console.log(rightBorderWidth);
